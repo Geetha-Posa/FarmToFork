f8db2ccceb482130116217ef38ff0fe16088a785
Geetha Posa <geethaposa20@gmail.com>
2025-08-02 17:28:59 +0530

    connected to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const connectDB = require('./config/db');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); //req.body
-const PORT = process.env.PORT || 3000;
+connectDB();
 
-app.listen(PORT ,() =>{
-    console.log('listening on the port 3000');
-})
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic test route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
